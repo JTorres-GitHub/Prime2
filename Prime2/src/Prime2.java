b77efc189c8c9030f90e8cb1b6e334f3a90e403a
Joaquin.Torres <torres.joaquin@gmail.com>
2020-06-12 10:43:21 -0400

    Initial code

---
+++ Prime2/src/Prime2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+// same as Prime1 project, but using a function instead
+
+public class Prime2 {
+	static boolean primeCheck(int num){
+		boolean flag = false;
+		boolean prime;
+		int i;
+		for (i = 2; i <= num/2; ++i){
+			if (num % i == 0){
+				flag = true;
+				break;
+			}
+		}
+		
+		if (!flag)
+			return prime = true;
+		else
+			return prime = false;
+	}
+	
+	public static void main(String[] args){
+		Scanner userInput = new Scanner(System.in);
+		int num;
+		boolean isPrime;
+		
+		System.out.println("Enter an integer: ");
+		num = userInput.nextInt();
+		
+		isPrime = primeCheck(num);
+		
+		if(!isPrime)
+			System.out.println(num + " is not a prime number.");
+		else
+			System.out.println(num + " is a prime number.");
+		
+	}
+}
